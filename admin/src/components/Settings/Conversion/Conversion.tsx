eed9ea0f3c6a3bc883706cb9cbb6e247ff58ed3c
Simnek <alexmitrovic993@gmail.com>
2023-11-16 23:26:54 +0100

    partial validation update

--- admin/src/components/Settings/Conversion/Conversion.tsx
+++ admin/src/components/Settings/Conversion/Conversion.tsx
@@ -12,6 +12,7 @@ import { LoadingIndicatorPage } from "@strapi/helper-plugin";
 import conversionRequests from "../../../api/conversionrate";
 import { IConversionRate } from "../../../../../types/conversionrate";
 import { currencies } from "currencies.json";
+import { Alert } from '@strapi/design-system';
 
 const initialData: IConversionRate = {
   id: 1,
@@ -20,11 +21,13 @@ const initialData: IConversionRate = {
   spread: ""
 };
 
-const Currency = () => {
+const Conversion = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isNew, setIsNew] = useState(true);
   const [data, setData] = useState<IConversionRate>(initialData)
   const [value, setValue] = useState<string | undefined>(undefined);
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [nosubmit, setNoSubmit] = useState<boolean>(false);
 
   const options = currencies.map((currency) => ({
     value: currency.code,
@@ -51,15 +54,23 @@ const Currency = () => {
       ...data,
       [name]: value,
     });
+    // Clear the error message when the user starts typing
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+    setNoSubmit(false);
   };
 
   const fetchData = async () => {
 
     try {
       const currency : any = await conversionRequests.getAllConversionRate();
-      setIsNew(false);
-      setData(currency);
-      setValue(currency.conversion_currency);
+      if(currency !== undefined) {
+        setIsNew(false);
+        setData(currency);
+        setValue(currency.conversion_currency);
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -68,8 +79,31 @@ const Currency = () => {
   };
 
   const saveCurrency = async (data: IConversionRate) => {
+    // Validate if all required fields are filled
+    const newErrors: Record<string, string> = {};
+    if (!data.conversion_currency) {
+      newErrors.conversion_currency = "Conversion Currency is required";
+    }
+    if (!data.rate) {
+      newErrors.rate = "Rate is required";
+    }
+    if (!data.spread) {
+      newErrors.spread = "Spread is required";
+    }
+
+    setErrors(newErrors);
+
+    // Check if there are any errors before saving
+    if (Object.keys(newErrors).length > 0) {
+      console.error("Please fill in all required fields");
+      return;
+    }
     if (!isNew) await conversionRequests.editConversion(data.id, data);
-    else await conversionRequests.addConversion(data);
+    else {
+      const conv = await conversionRequests.addConversion(data);
+      console.log("conv", conv)
+      setIsNew(false)
+    }
 
     await fetchData();
   };
@@ -78,7 +112,10 @@ const Currency = () => {
   return (
     <Layout>
       <ContentLayout>
-        <Box padding="2rem">
+        {nosubmit &&<Alert closeLabel="Close" onClose={() => setNoSubmit(false)} title="Error" variant="danger">
+          Fill all required fields.
+        </Alert>}
+        <Box padding="3rem">
           <Typography variant="title">Store conversion currency</Typography>
 
           <Box marginTop="1rem">
@@ -90,6 +127,7 @@ const Currency = () => {
               }}
               value={value}
               onChange={handleSingleSelectChange}
+              required
             >
               {options.map((option) => (
                 <SingleSelectOption key={option.value} value={option.value}>
@@ -97,14 +135,19 @@ const Currency = () => {
                 </SingleSelectOption>
               ))}
             </SingleSelect>
-            <TextInput
+            {errors.conversion_currency && (
+              <Typography textColor="danger600">{errors.conversion_currency}</Typography>
+            )}
+              <TextInput
               name="rate"
               value={data.rate}
               onChange={handleInputChange}
               fullWidth
               placeholder="Rate"
               label="Rate"
+              required
             />
+            {errors.rate && <Typography textColor="danger600">{errors.rate}</Typography>}
             <TextInput
               name="spread"
               value={data.spread}
@@ -112,7 +155,9 @@ const Currency = () => {
               fullWidth
               placeholder="Spread"
               label="Spread"
+              required
             />
+            {errors.spread && <Typography textColor="danger600">{errors.spread}</Typography>}
           </Box>
           <br/>
           <Button onClick={() => saveCurrency(data)} variant="secondary">
@@ -124,4 +169,4 @@ const Currency = () => {
   );
 };
 
-export default Currency;
+export default Conversion;
